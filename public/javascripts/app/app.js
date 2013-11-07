1986e4fe7800325d81f20344c1ad0d5cb8215001
Pete Dunlap <petedunlap@gmail.com>
2013-11-07 12:13:40 -0600

    tile generation in app.js

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -3,6 +3,7 @@ $(document).ready(initialize);
 function initialize(){
   $(document).foundation();
   $('#newGame').on('click', clickNewGame);
+  $('#gameDisplay').on('click','.tile',clickTile);//add class of clicked flip it,
 }
 
 
@@ -14,4 +15,16 @@ function clickNewGame(){
   squares = _.shuffle(squares);
   console.log(player,numOfTiles,squares);
   $('#gameDisplay').show();
+  for(var i = 0; i<squares.length;i++){
+    var $tile = $('<div>')
+    $tile.attr('data-position', i);
+    $tile.addClass('tile');
+    $('#gameDisplay').append($tile);
+  }
+
+}
+
+
+function clickTile(){
+
 }
