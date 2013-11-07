c9309a924b228827e1070f773a4698b6662bd011
Pete Dunlap <petedunlap@gmail.com>
2013-11-07 10:54:41 -0600

    initial commit

---
+++ routes/games.js
@@ -0,0 +1,14 @@
+var colors = require('colors');
+// Colors
+// bold, italic, underline, inverse, yellow, cyan,
+// white, magenta, green, red, grey, blue, rainbow,
+// zebra, random
+
+/*
+ * GET /
+ */
+
+exports.index = function(req, res){
+  console.log('games.index'.italic.underline.bold.magenta);
+  res.render('games/index', {title: 'Express'});
+};
