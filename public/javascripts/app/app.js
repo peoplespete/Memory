bb988627e0def5f93387ecb4be501f724e48a5d0
Pete Dunlap <petedunlap@gmail.com>
2013-11-07 11:54:37 -0600

    funkyness resolved

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -7,11 +7,11 @@ function initialize(){
 
 
 function clickNewGame(){
-  var player = $('#player').val();
-  var numOfTiles = $('#numOfTiles').val();
-  var squares = _.range(10);
+  var player = $('input[name="player"]').val();
+  var numOfTiles = parseInt($('input[name="numOfTiles"]').val());
+  var squares = _.range(numOfTiles);
   squares = squares.concat(squares);
   squares = _.shuffle(squares);
-
+  console.log(player,numOfTiles,squares);
   $('#gameDisplay').show();
 }
